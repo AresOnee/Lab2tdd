71b7796e19476e5f1af309f89a4c312392df162b
VV <vladi.vidal.l@gmail.com>
2026-01-21 15:42:02 -0300

    add

--- FrontEnd/src/router/index.js
+++ FrontEnd/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { authService } from '@/services/authService'
 
+import RoutesView from '../views/RoutesView.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -92,6 +94,13 @@ const router = createRouter({
       name: 'statistics',
       component: () => import('@/views/StatisticsView.vue'),
       meta: { requiresAuth: true }
+    },
+    
+    {
+       path: '/rutas',
+       name: 'rutas',
+       component: RoutesView,
+       meta: { requiresAuth: true }
     }
   ]
 })
@@ -99,11 +108,11 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   const isAuthenticated = authService.isAuthenticated()
 
-  // Rutas que requieren autenticación
+  
   if (to.meta.requiresAuth && !isAuthenticated) {
     next({ name: 'login' })
   }
-  // Rutas solo para invitados (login/register)
+  
   else if (to.meta.requiresGuest && isAuthenticated) {
     next({ name: 'home' })
   }
